86677999073427ba4d213f164a7d2e5f1b3552c3
Rashid-Ali-Kalhoro <87558577+Rashid-Ali-Kalhoro@users.noreply.github.com>
2024-05-29 17:55:31 +0500

    logout

--- src/App.js
+++ src/App.js
@@ -4,15 +4,27 @@ import Home from './pages/Home';
 import CreatePost from './pages/CreatePost';
 import Login from './pages/Login';
 import { useState } from 'react';
+import { auth } from './firebase-config';
+import { signOut } from 'firebase/auth';
+
 
 function App() {
   const [isAuth, setIsAuth] = useState(false);
+
+  const signUserOut = () => {
+    signOut(auth).then(() => {
+      localStorage.clear();
+      setIsAuth(false);
+      window.location.pathname = "/login";
+    });
+  };
+
   return (
     <Router>
       <nav>
         <Link to="/" > Home </Link>
         <Link to="/createpost"> Create Post </Link>
-        <Link to="/login" > Login </Link>
+        { !isAuth ? <Link to="/login" > Login </Link> : <button onClick={signUserOut}> Log out </button> }
       </nav>
       <Routes>
         <Route path='/' element={<Home />} />
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { auth, provider } from '../firebase-config';
 import { signInWithPopup } from 'firebase/auth';
+import { useNavigate } from 'react-router-dom';
 
 function Login({ setIsAuth }) {
+  let navigate = useNavigate();
+
    const signInWithGoogle = () => {
       signInWithPopup(auth, provider).then((result) => {
         localStorage.setItem("isAuth", true);
         setIsAuth(true);
+        navigate("/");
       });
    };
 
